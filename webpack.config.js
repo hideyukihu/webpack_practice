431a6139dda204f6755325f4067de904f96ec14f
hide <92362830+hideyukihu@users.noreply.github.com>
2022-03-05 13:47:38 +0900

    Generate Html

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,20 @@ module.exports = {
                     //     }
                     // }
                 ]
+            },
+            {
+                test: /\.pug/,
+                use: [{
+                    loader: 'html-loader',
+        
+                },
+                {
+                    loader: 'pug-html-loader',
+                    options: {
+                        pretty: true,
+                    }
+                }
+            ]
             }
         ]
     },
@@ -45,8 +59,13 @@ module.exports = {
             filename: './stylesheets/main.css'
         }),
         new HtmlWebpackPlugin({
-            template: './src/template/index.html'
+            template: './src/template/index.pug',
+            filename: 'index.html'
         }),
+        new HtmlWebpackPlugin({
+            template: './src/template/access.pug',
+            filename: 'access.html'
+                }),
         new CleanWebpackPlugin(),
     ],
 }
