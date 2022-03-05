3861736b625a2091b34d75670c289550fc5a8906
hide <92362830+hideyukihu@users.noreply.github.com>
2022-03-05 17:12:43 +0900

    add babel-loader options @babel/preset-env

--- src/javascript/my.js
+++ src/javascript/my.js
@@ -1,4 +1,9 @@
 export default function() {
-    console.log("this is module");
+    const obj = { a: 1, b: 2};
+    const newObj = {
+        ...obj, c: 3
+    };
+    console.log('this is module', newObj);
+    
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,21 @@ module.exports = {
     module: {
         rules:[
             {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', { 'targets': '> 30%, not dead' }],
+                            ],
+                        }
+                    }
+                ],
+            },
+            
+            {
                 test: /\.(css|sass|scss)/,
                 use: [
                     {
